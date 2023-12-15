e8c978fa8d312aa4ee62d920353904f41db7381b
DrFlowerkick <flowerkick14@gmail.com>
2023-12-15 02:16:42 +0100

    Now really :()

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ pub mod days;
 use anyhow::Result;
 
 pub fn run() -> Result<()> {
-    let input_01 = include_str!("../assets/input_01.txt");
+    let input_01 = include_str!("../assets/day_01.txt");
     days::day_01::day_01(input_01)?;
-    days::day_02::day_02(input_01)?;
+    //days::day_02::day_02(input_01)?;
     
     Ok(())
 }
