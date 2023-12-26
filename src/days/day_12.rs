511372361bacdb9146c46a396636645bfa0273b9
DrFlowerkick <flowerkick14@gmail.com>
2023-12-26 17:25:32 +0100

    Completed Day 12, solution inspired by HyperNeutrino (for links see code commentary)

--- src/days/day_12.rs
+++ src/days/day_12.rs
@@ -1,40 +1,8 @@
 //!day_12.rs
 
+use std::collections::HashMap;
+
 use anyhow::Result;
-
-trait Spring {
-    fn is_spring(&self) -> bool;
-    fn is_operational(&self) -> bool;
-    fn is_or_may_be_undamaged(&self) -> bool;
-    fn is_or_may_be_damaged(&self) -> bool;
-}
-
-impl Spring for char {
-    fn is_spring(&self) -> bool {
-        match self {
-            '.' | '#' | '?' => true,
-            _ => false,
-        }
-    }
-    fn is_operational(&self) -> bool {
-        match self {
-            '.' => true,
-            _ => false,
-        }
-    }
-    fn is_or_may_be_undamaged(&self) -> bool {
-        match self {
-            '.' | '?' => true,
-            _ => false,
-        }
-    }
-    fn is_or_may_be_damaged(&self) -> bool {
-        match self {
-            '#' | '?' => true,
-            _ => false,
-        }
-    }
-}
 
 fn springs_and_damaged_clusters(input: &str) -> (&str, Vec<usize>) {
     input
@@ -44,13 +12,14 @@ fn springs_and_damaged_clusters(input: &str) -> (&str, Vec<usize>) {
             if springs
                 .trim()
                 .chars()
-                .filter(|c| !c.is_spring())
+                .filter(|c| !*(&['.', '#', '?'].contains(c)))
                 .next()
                 .is_some()
             {
                 panic!("bad spring char");
             }
             let damaged_clusters: Vec<usize> = d
+                .trim()
                 .split(',')
                 .map(|s| s.parse::<usize>().expect("bad input"))
                 .collect();
@@ -59,82 +28,112 @@ fn springs_and_damaged_clusters(input: &str) -> (&str, Vec<usize>) {
         .unwrap()
 }
 
-fn get_max_range_end(springs: &str, damaged_clusters: &[usize]) -> usize {
-    springs.len() - if damaged_clusters.len() == 1 {
-        0
-    } else {
-        damaged_clusters[1..].len() + damaged_clusters[1..].iter().sum::<usize>()
+fn unfold_springs_and_damaged_clusters(
+    springs: &str,
+    damaged_clusters: &[usize],
+) -> (String, Vec<usize>) {
+    let mut unfolded_springs = springs.to_string();
+    let mut unfolded_damaged_clusters = damaged_clusters.to_vec();
+    for _i in 0..4 {
+        unfolded_springs = unfolded_springs + "?" + springs;
+        unfolded_damaged_clusters.extend_from_slice(damaged_clusters);
     }
+    (unfolded_springs, unfolded_damaged_clusters)
 }
 
-fn different_arrangements(springs: &str, damaged_clusters: &[usize]) -> usize {
-    let max_range_end = get_max_range_end(springs, damaged_clusters);
-    let mut index = 0;
-    let mut num_different_arrangements = 0;
-    loop {
-        match springs
-            .chars()
-            .enumerate()
-            .position(|(i, s)| i >= index && s.is_or_may_be_damaged())
-        {
-            Some(range_start) => {
-                let range_end = range_start + damaged_clusters[0];
-                if range_end > max_range_end {
-                    // cluster(s) cannot fit
-                    return num_different_arrangements;
-                }
-                if springs[range_start..range_end]
-                    .chars()
-                    .filter(|s| s.is_operational())
-                    .next()
-                    .is_none()
-                    && (range_start == 0
-                        || springs
-                            .chars()
-                            .nth(range_start - 1)
-                            .unwrap()
-                            .is_or_may_be_undamaged())
-                    && (range_end == springs.len()
-                        || springs
-                            .chars()
-                            .nth(range_end)
-                            .unwrap()
-                            .is_or_may_be_undamaged())
-                {
-                    // cluster fit's
-                    if damaged_clusters.len() == 1 {
-                        num_different_arrangements += 1;
-                    } else if range_end + 1 >= springs.len() {
-                        // no more springs left after current cluster
-                        return num_different_arrangements;
-                    } else {
-                        // search for next cluster
-                        num_different_arrangements += different_arrangements(
-                            &springs[(range_end + 1)..],
-                            &damaged_clusters[1..],
-                        );
-                    }
-                }
-                index = range_start + 1;
-            }
-            None => return num_different_arrangements,
+
+// this solution is an implementation in Rust of https://www.youtube.com/watch?v=g3Ms5e7Jdqo by HyperNeutrino
+// www.youtube.com/@hyper-neutrino
+// github.com/hyper-neutrino
+fn different_arrangements(
+    springs: &str,
+    damaged_clusters: &[usize],
+    cache: &mut HashMap<(String, Vec<usize>), usize>,
+) -> usize {
+    // no more springs left
+    if springs.len() == 0 {
+        if damaged_clusters.len() == 0 {
+            return 1;
+        } else {
+            return 0;
         }
     }
+    // no more damaged clusters left
+    if damaged_clusters.len() == 0 {
+        if springs.contains('#') {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // if in cache, returned cached result
+    if let Some(cached_num_different_arrangements) =
+        cache.get(&(springs.to_string(), damaged_clusters.to_vec()))
+    {
+        return *cached_num_different_arrangements;
+    }
+
+    // search for different configurations
+    let mut num_different_arrangements = 0;
+
+    // pretend '?' as operational spring
+    if springs[0..1].contains(&['.', '?']) {
+        num_different_arrangements +=
+            different_arrangements(&springs[1..], damaged_clusters, cache);
+    }
+
+    // pretend '?' as damaged spring
+    if springs[0..1].contains(&['#', '?']) {
+        if damaged_clusters[0] <= springs.chars().count()
+            && !springs[..damaged_clusters[0]].contains('.')
+        {
+            if damaged_clusters[0] == springs.chars().count() {
+                // if end of springs, call different_arrangements with "" to check len() of damaged_clusters
+                num_different_arrangements +=
+                    different_arrangements("", &damaged_clusters[1..], cache)
+            } else if springs.chars().nth(damaged_clusters[0]).unwrap() != '#' {
+                // if remaining springs, call different_arrangements with remaining springs and damaged_clusters
+                num_different_arrangements += different_arrangements(
+                    &springs[(damaged_clusters[0] + 1)..],
+                    &damaged_clusters[1..],
+                    cache,
+                );
+            }
+        }
+    }
+    // insert current result into cache for later use
+    cache.insert(
+        (springs.to_string(), damaged_clusters.to_vec()),
+        num_different_arrangements,
+    );
+
+    num_different_arrangements
 }
 
 pub fn day_12() -> Result<()> {
     let input = include_str!("../../assets/day_12.txt");
     let mut sum_different_arrangements = 0;
+    let mut sum_different_arrangements_unfolded = 0;
+    let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
     for line in input.lines() {
         let (springs, damaged_clusters) = springs_and_damaged_clusters(line);
-        let line_arrangements = different_arrangements(&springs[..], &damaged_clusters[..]);
-        if line_arrangements == 0 {
-            eprintln!("line_arrangements == 0 at {} {:?}", springs, damaged_clusters);
-        }
-        sum_different_arrangements += line_arrangements;
+        sum_different_arrangements +=
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
+        let (unfolded_springs, unfolded_damaged_clusters) =
+            unfold_springs_and_damaged_clusters(springs, &damaged_clusters);
+        sum_different_arrangements_unfolded += different_arrangements(
+            unfolded_springs.as_str(),
+            &unfolded_damaged_clusters[..],
+            &mut cache,
+        );
     }
 
     println!("result day 12 part 1: {}", sum_different_arrangements);
+    println!(
+        "result day 12 part 2: {}",
+        sum_different_arrangements_unfolded
+    );
 
     Ok(())
 }
@@ -145,13 +144,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_arrangements_01() {
+    fn test_arrangements() {
         let input = "???.### 1,1,3";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 01 result day 12 part 1: {}",
             test_different_arrangements
@@ -163,10 +163,11 @@ mod tests {
     fn test_arrangements_02() {
         let input = ".??..??...?##. 1,1,3";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 02 result day 12 part 1: {}",
             test_different_arrangements
@@ -178,10 +179,11 @@ mod tests {
     fn test_arrangements_03() {
         let input = "?#?#?#?#?#?#?#? 1,3,1,6";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 03 result day 12 part 1: {}",
             test_different_arrangements
@@ -193,10 +195,11 @@ mod tests {
     fn test_arrangements_04() {
         let input = "????.#...#... 4,1,1";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 04 result day 12 part 1: {}",
             test_different_arrangements
@@ -208,10 +211,11 @@ mod tests {
     fn test_arrangements_05() {
         let input = "????.######..#####. 1,6,5";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 05 result day 12 part 1: {}",
             test_different_arrangements
@@ -223,10 +227,11 @@ mod tests {
     fn test_arrangements_06() {
         let input = "?###???????? 3,2,1";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 06 result day 12 part 1: {}",
             test_different_arrangements
@@ -238,59 +243,79 @@ mod tests {
     fn test_arrangements_07() {
         let input = ".#??#..???#..?? 2,2,1";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 07 result day 12 part 1: {}",
             test_different_arrangements
         );
-        assert_eq!(test_different_arrangements, 10);
+        assert_eq!(test_different_arrangements, 0);
     }
 
     #[test]
     fn test_arrangements_08() {
         let input = ".#??#..???#..?? 2,1,2";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 08 result day 12 part 1: {}",
             test_different_arrangements
         );
-        assert_eq!(test_different_arrangements, 12);
+        assert_eq!(test_different_arrangements, 1);
     }
 
     #[test]
     fn test_arrangements_09() {
         let input = "????#?.??? 2,1,1";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 09 result day 12 part 1: {}",
             test_different_arrangements
         );
-        assert_eq!(test_different_arrangements, 10);
+        assert_eq!(test_different_arrangements, 8);
     }
 
     #[test]
     fn test_arrangements_10() {
         let input = "??.??????#???#?????# 1,1,7,3,1";
         let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
         eprintln!("{}", springs);
         eprintln!("{:?}", damaged_clusters);
         let test_different_arrangements =
-            different_arrangements(&springs[..], &damaged_clusters[..]);
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
         println!(
             "test 10 result day 12 part 1: {}",
             test_different_arrangements
         );
-        assert_eq!(test_different_arrangements, 13);
+        assert_eq!(test_different_arrangements, 9);
+    }
+
+    #[test]
+    fn test_arrangements_11() {
+        let input = ".##.?#??.#.?# 2,1,1,1";
+        let (springs, damaged_clusters) = springs_and_damaged_clusters(input);
+        let mut cache: HashMap<(String, Vec<usize>), usize> = HashMap::new();
+        eprintln!("{}", springs);
+        eprintln!("{:?}", damaged_clusters);
+        let test_different_arrangements =
+            different_arrangements(&springs[..], &damaged_clusters[..], &mut cache);
+        println!(
+            "test 11 result day 12 part 1: {}",
+            test_different_arrangements
+        );
+        assert_eq!(test_different_arrangements, 1);
     }
 }
